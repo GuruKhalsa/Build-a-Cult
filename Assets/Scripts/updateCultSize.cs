ee870128c2a8f483401d86ff970b73d6d32ff52b
Drew <drew@kryptnostic.com>
2016-01-30 16:17:38 -0800

    hud and scripts

---
+++ Assets/Scripts/updateCultSize.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace AssemblyCSharp
+{
+	public class updateCultSize : MonoBehaviour
+	{
+		public static int score;
+
+		Text scoreText;
+
+		void Awake ()
+		{
+			scoreText = GetComponent<Text> ();
+		}
+
+		// Use this for initialization
+		void Start ()
+		{
+			score = 0;
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+			scoreText.text = "Cult Size: " + score;
+			score++;
+		}
+	}
+}
