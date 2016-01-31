85203286bba8954c94e17492e0574db31dd79d9f
Drew <drew@kryptnostic.com>
2016-01-30 18:52:21 -0800

    remove old prefabs, update controller input and player

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -1,109 +1,51 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class ControllerInput : MonoBehaviour {
+namespace AssemblyCSharp
+{
+	public class ControllerInput : MonoBehaviour
+	{
+		public KeyCode jumpButton = KeyCode.Space;
+		public KeyCode attackButton = KeyCode.F;
+		public KeyCode actionButton = KeyCode.G;
 
-	public KeyCode jumpButton = KeyCode.Space;
-	public KeyCode attackButton = KeyCode.F;
-	public KeyCode actionButton = KeyCode.G;
+		public bool actionButtonDown ()
+		{
+			return Input.GetKeyDown (actionButton);
+		}
 
+		public bool JumpDown ()
+		{
+			return Input.GetKeyDown (jumpButton);
+		}
 
-	// Use this for initialization
-	void Start () {
-	
+		public bool attacking ()
+		{
+			return Input.GetKey (attackButton);
+		}
+
+		public bool attackDown ()
+		{
+			return Input.GetKeyDown (attackButton);
+		}
+
+		public bool attackUp ()
+		{
+			return Input.GetKeyUp (attackButton);
+		}
+
+		public bool jumpUp ()
+		{
+			return Input.GetKeyUp (jumpButton);
+		}
+
+		public float movementX ()
+		{
+			return Input.GetAxis ("Horizontal");
+		}
+
+		public float movementY ()
+		{
+			return Input.GetAxis ("Vertical");
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	public bool actionButtonDown(){
-	
-	
-		if (Input.GetKeyDown (actionButton))
-			return true;
-		else
-			return false;
-	
-	}
-
-
-
-
-
-	public bool JumpDown(){
-
-
-		if (Input.GetKeyDown (jumpButton))
-			return true;
-		else
-			return false;
-
-	}
-
-
-	public bool attacking(){
-
-		if (Input.GetKey (attackButton))
-			return true;
-		else
-			return false;
-
-
-	}
-
-
-	public bool attackDown(){
-
-		if (Input.GetKeyDown (attackButton))
-			return true;
-		else
-			return false;
-
-
-
-	}
-
-	public bool attackUp(){
-
-		if (Input.GetKeyUp (attackButton))
-			return true;
-		else
-			return false;
-
-
-	}
-
-
-	public bool jumpUp(){
-
-		if (Input.GetKeyUp (jumpButton))
-			return true;
-		else
-			return false;
-
-
-
-	}
-
-
-
-	public float movementX(){
-
-		return Input.GetAxis ("X1");
-
-
-	}
-
-	public float movementY(){
-		
-		return Input.GetAxis ("Y1");
-
-
-	}
-
-
-
-
 }
